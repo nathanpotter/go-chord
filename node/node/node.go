b070c9f7528082048416aca50725362d35953134
Nathan Potter <nrpotter15@gmail.com>
2016-09-02 16:32:52 -0500

    Adding node.findMyNode

--- node/node/node.go
+++ node/node/node.go
@@ -5,6 +5,7 @@
 package node
 
 import (
+  "strings"
   pb "github.com/nathanpotter/go-chord/node/protos"
 )
 
@@ -12,8 +13,24 @@ type node struct {
   this *pb.Nodes_Node
 }
 
-func NewNode() *node {
-  return &node{
-    this: &pb.Nodes_Node{},
+func NewNode(ip, port string) *node {
+  if !strings.HasPrefix(port, ":") {
+    port = ":" + port
   }
+  return &node{
+    this: &pb.Nodes_Node{Ip: ip, Port: port},
+  }
+}
+
+func (n *node) findMyNode(nodes *pb.Nodes) *pb.Nodes_Node {
+  for _, node := range nodes.Nodes {
+    if node.Ip == n.this.Ip && node.Port == n.this.Port {
+      return node
+    }
+  }
+  return nil
+}
+
+func (n *node) UpdateDHT(ctx context.Context, nodes *pb.Nodes) (*pb.Empty, error) {
+  
 }
--- node/node/node_test.go
+++ node/node/node_test.go
@@ -2,18 +2,44 @@ package node
 
 import (
   "testing"
+
+  pb "github.com/nathanpotter/go-chord/node/protos"
+  "github.com/golang/protobuf/proto"
 )
 
 var (
   n *node
+  nodes *pb.Nodes
 )
 
 func TestNewNode(t *testing.T) {
-  n = NewNode()
+  n = NewNode("127.0.0.1", "5050")
   if n == nil {
     t.Fatalf("NewNode should not return nil")
   }
   if n.this == nil {
     t.Fatalf("n.this should not be nil after returning from NewNode")
   }
+}
+
+func TestFindMyNode(t *testing.T) {
+  nodes := &pb.Nodes{
+    Nodes: []*pb.Nodes_Node{
+      &pb.Nodes_Node{Ip: "192.76.91.5", Port:":9000", Id: 3},
+      &pb.Nodes_Node{Ip: "127.0.0.1", Port:":5050", Id: 28},
+      &pb.Nodes_Node{Ip: "176.80.20.18", Port:":9090", Id: 41},
+      &pb.Nodes_Node{Ip: "176.58.21.37", Port:":8000", Id: 19},
+      &pb.Nodes_Node{Ip: "192.54.38.76", Port:":6000", Id: 56},
+    },
+  }
+  ns := nodes.GetNodes()
+  myNode := ns[1]
+  n.this = n.findMyNode(nodes)
+  if !proto.Equal(n.this, myNode) {
+    t.Errorf("Should replace n.this with correct node which has Id", n.this, myNode)
+  }
+}
+
+func TestUpdateDHT(t *testing.T) {
+  
 }
