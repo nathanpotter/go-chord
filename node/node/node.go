8c1e0be586afc6d9ecd017ea624d5359e2f515e0
Nathan Potter <nrpotter15@gmail.com>
2016-09-02 23:44:06 -0500

    Adding fingertable/node routing logic

--- node/node/node.go
+++ node/node/node.go
@@ -5,32 +5,101 @@
 package node
 
 import (
-  "strings"
-  pb "github.com/nathanpotter/go-chord/node/protos"
+	"errors"
+	"strings"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/nathanpotter/go-chord/node/protos"
+)
+
+var (
+	NilNodesError = errors.New("Invalid argument, nodes cannot be nil")
+)
+
+const (
+	m         = 6
+	hashSpace = 2 << (m - 1) // 2^6 nodes allowed in the system
 )
 
 type node struct {
-  this *pb.Nodes_Node
+	this    *pb.Nodes_Node
+	fingers []*pb.Nodes_Node
 }
 
 func NewNode(ip, port string) *node {
-  if !strings.HasPrefix(port, ":") {
-    port = ":" + port
-  }
-  return &node{
-    this: &pb.Nodes_Node{Ip: ip, Port: port},
-  }
+	if !strings.HasPrefix(port, ":") {
+		port = ":" + port
+	}
+	return &node{
+		this:    &pb.Nodes_Node{Ip: ip, Port: port},
+		fingers: make([]*pb.Nodes_Node, m, m),
+	}
 }
 
 func (n *node) findMyNode(nodes *pb.Nodes) *pb.Nodes_Node {
-  for _, node := range nodes.Nodes {
-    if node.Ip == n.this.Ip && node.Port == n.this.Port {
-      return node
-    }
-  }
-  return nil
+	for _, node := range nodes.Nodes {
+		if node.Ip == n.this.Ip && node.Port == n.this.Port {
+			return node
+		}
+	}
+	return nil
 }
 
 func (n *node) UpdateDHT(ctx context.Context, nodes *pb.Nodes) (*pb.Empty, error) {
-  
+	err := n.updateDHT(nodes)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (n *node) updateDHT(nodes *pb.Nodes) error {
+	if nodes == nil || nodes.Nodes == nil {
+		return NilNodesError
+	}
+  for i := 0; i<m; i++ {
+    var val uint64
+    if i == 0 {
+      val = ((n.this.Id + 1) % hashSpace)
+    } else {
+      val = ((n.this.Id + (2 << uint64((i-1)))) % hashSpace)
+    }
+    node, err := findSuccessor(val, nodes)
+    if err != nil {
+      return err
+    }
+    n.fingers[i] = node
+  }
+	return nil
+}
+
+// Finds successor to id in the nodes set. Rolls over to min node if no node is
+// between id and upper limit of hashSpace.
+func findSuccessor(id uint64, nodes *pb.Nodes) (*pb.Nodes_Node, error) {
+  var n *pb.Nodes_Node
+  var min *pb.Nodes_Node
+
+	if nodes == nil || nodes.Nodes == nil {
+		return nil, NilNodesError
+	}
+
+  for _, node := range nodes.Nodes {
+    if node.Id > id && n == nil {
+      n = node
+    } else if node.Id > id && node.Id < n.Id {
+      n = node
+    } else if node.Id < id && min == nil {
+      min = node
+    } else if node.Id < id && node.Id < min.Id {
+      min = node
+    } else {
+      continue
+    }
+  }
+
+  if n == nil {
+    return min, nil
+  }
+	return n, nil
 }
--- node/node/node_test.go
+++ node/node/node_test.go
@@ -1,45 +1,129 @@
 package node
 
 import (
-  "testing"
+	"testing"
 
-  pb "github.com/nathanpotter/go-chord/node/protos"
-  "github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/proto"
+	pb "github.com/nathanpotter/go-chord/node/protos"
 )
 
 var (
-  n *node
-  nodes *pb.Nodes
+	n     *node
+	nodes = &pb.Nodes{
+		Nodes: []*pb.Nodes_Node{
+			&pb.Nodes_Node{Ip: "192.76.91.5", Port: ":9000", Id: 3},
+			&pb.Nodes_Node{Ip: "127.0.0.1", Port: ":5050", Id: 28},
+			&pb.Nodes_Node{Ip: "176.80.20.18", Port: ":9090", Id: 41},
+			&pb.Nodes_Node{Ip: "176.58.21.37", Port: ":8000", Id: 19},
+			&pb.Nodes_Node{Ip: "192.54.38.76", Port: ":6000", Id: 56},
+		},
+	}
 )
 
 func TestNewNode(t *testing.T) {
-  n = NewNode("127.0.0.1", "5050")
-  if n == nil {
-    t.Fatalf("NewNode should not return nil")
-  }
-  if n.this == nil {
-    t.Fatalf("n.this should not be nil after returning from NewNode")
-  }
+	n = NewNode("127.0.0.1", "5050")
+	if n == nil {
+		t.Fatalf("NewNode should not return nil")
+	}
+	if n.this == nil {
+		t.Fatalf("n.this should not be nil after returning from NewNode")
+	}
+	if n.fingers == nil {
+		t.Fatalf("n.fingers should not be nil after returning from NewNode")
+	}
 }
 
 func TestFindMyNode(t *testing.T) {
-  nodes := &pb.Nodes{
-    Nodes: []*pb.Nodes_Node{
-      &pb.Nodes_Node{Ip: "192.76.91.5", Port:":9000", Id: 3},
-      &pb.Nodes_Node{Ip: "127.0.0.1", Port:":5050", Id: 28},
-      &pb.Nodes_Node{Ip: "176.80.20.18", Port:":9090", Id: 41},
-      &pb.Nodes_Node{Ip: "176.58.21.37", Port:":8000", Id: 19},
-      &pb.Nodes_Node{Ip: "192.54.38.76", Port:":6000", Id: 56},
-    },
+
+	ns := nodes.GetNodes()
+	myNode := ns[1]
+	n.this = n.findMyNode(nodes)
+	if !proto.Equal(n.this, myNode) {
+		t.Errorf("Should replace n.this with correct node which has Id", n.this, myNode)
+	}
+}
+
+func TestNilNodesUpdateDHT(t *testing.T) {
+  // UpdateDHT
+  _, err := n.UpdateDHT(nil, nil)
+  if err == nil {
+    t.Errorf("Should receive NilNodesError when calling UpdateDHT with nil nodes")
   }
-  ns := nodes.GetNodes()
-  myNode := ns[1]
-  n.this = n.findMyNode(nodes)
-  if !proto.Equal(n.this, myNode) {
-    t.Errorf("Should replace n.this with correct node which has Id", n.this, myNode)
+  _, err = n.UpdateDHT(nil, &pb.Nodes{})
+  if err == nil {
+    t.Errorf("Should receive NilNodesError when calling UpdateDHT with nil nodes.Nodes")
+  }
+
+  // updateDHT
+  err = n.updateDHT(nil)
+  if err == nil {
+    t.Errorf("Should receive NilNodesError when calling updateDHT with nil nodes")
+  }
+  err = n.updateDHT(&pb.Nodes{})
+  if err == nil {
+    t.Errorf("Should receive NilNodesError when calling updateDHT with nil nodes.Nodes")
   }
 }
 
 func TestUpdateDHT(t *testing.T) {
-  
+  n.UpdateDHT(nil, nodes)
+  if n.fingers[0] == nil {
+    t.Errorf("node.fingers shouldn't be nil after updateDHT")
+  }
+  if !proto.Equal(n.fingers[0], nodes.Nodes[2]) {
+    t.Errorf("n.fingers[0] should be equal to nodes.Nodes[2]")
+  }
+  if !proto.Equal(n.fingers[1], nodes.Nodes[2]) {
+    t.Errorf("n.fingers[1] should be equal to nodes.Nodes[2]")
+  }
+  if !proto.Equal(n.fingers[5], nodes.Nodes[0]) {
+    t.Errorf("n.fingers[5] should be equal to nodes.Nodes[0]")
+  }
+}
+
+func TestFindSuccessor(t *testing.T) {
+	// nil nodes
+	node, err := findSuccessor(0, nil)
+	if err == nil {
+		t.Errorf("Should receive error from nil nodes argument")
+	}
+	if node != nil {
+		t.Errorf("Should receive nil node when nodes argument is nil")
+	}
+
+	node, err = findSuccessor(0, nodes)
+	if err != nil {
+		t.Errorf("Shouldn't receive error from valid findSuccessor call")
+	}
+	if node == nil {
+		t.Errorf("Shouldn't receive nil node when nodes argument is valid")
+	}
+	if !proto.Equal(node, nodes.Nodes[0]) {
+		t.Errorf("n should equal nodes.Nodes[0] when id = 0")
+	}
+
+	node, err = findSuccessor(3, nodes)
+	if err != nil {
+		t.Errorf("Shouldn't receive error from valid findSuccessor call")
+	}
+	if !proto.Equal(node, nodes.Nodes[3]) {
+		t.Errorf("n should equal nodes.Nodes[3] when id = 3")
+	}
+
+	node, err = findSuccessor(25, nodes)
+	if err != nil {
+		t.Errorf("Shouldn't receive error from valid findSuccessor call")
+	}
+	if !proto.Equal(node, nodes.Nodes[1]) {
+		t.Errorf("n should equal nodes.Nodes[1] when id = 25")
+	}
+
+	node, err = findSuccessor(60, nodes)
+	if err != nil {
+		t.Errorf("Shouldn't receive error from valid findSuccessor call")
+	}
+	if !proto.Equal(node, nodes.Nodes[0]) {
+		t.Errorf("n should equal nodes.Nodes[0] when id = 60")
+	}
+
 }
